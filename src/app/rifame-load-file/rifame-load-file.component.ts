0666d27ab59358c003e3e71cb663e460d8601219
Ricardo Brito Brens <britobrensricardo@gmail.com>
2021-11-23 23:41:41 -0400

    [ADD] raw reading of excel file

--- src/app/rifame-load-file/rifame-load-file.component.ts
+++ src/app/rifame-load-file/rifame-load-file.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-rifame-load-file',
   templateUrl: './rifame-load-file.component.html',
@@ -9,10 +9,30 @@ export class RifameLoadFileComponent implements OnInit {
 
   @Input()
   visibility: boolean = false;
-
+  convertedJson: String;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  fileUpload(event:any){
+    console.info(event.target.files);
+
+    const selectedFile = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsBinaryString(selectedFile);
+    fileReader.onloadend = (event) =>
+    {
+      console.info(event);
+      let binaryData = event.target.result;
+      let workbook = XLSX.read(binaryData, {type:'binary'});
+      workbook.SheetNames.forEach(sheet => {
+        const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+        console.log(data);
+        this.convertedJson = JSON.stringify(data, undefined,4);
+
+      })
+      console.info(workbook);
+    }
+  }
 }
